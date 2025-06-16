73d1945a86e186e8898cd3a7c26f2df6c98fc6c9
camel <206016544+camelneg@users.noreply.github.com>
2025-06-16 21:13:24 +0200

    Update payload_example.js

--- payload_example.js
+++ payload_example.js
@@ -1,11 +1,2 @@
-/*
-              ___                           
-  ___  _ __  |   \ ___ __ _ _ __  ___ _ _  
- / _ \| '  \ | |) / -_) _` | '  \/ -_) '_| 
- \___/|_|_|_||___/\___\__,_|_|_|_\___|_|   
-      Payload Demo - by apologygirl0       
-      Action: Lancer calc.exe Windows       
-*/
-
 var shell = new ActiveXObject("WScript.Shell");
-shell.Run("calc.exe", 0, false);  // 0 = cache la fenêtre cmd
+shell.Run("calc.exe", 0, false);
